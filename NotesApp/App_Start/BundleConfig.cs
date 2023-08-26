ed457497f036c0f91dbe09368f2051e5808dd1a6
Tamara Kostova <tamarakostova.bt@gmail.com>
2023-08-26 17:51:39 +0200

    style

--- NotesApp/App_Start/BundleConfig.cs
+++ NotesApp/App_Start/BundleConfig.cs
@@ -22,6 +22,10 @@ namespace NotesApp
             bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new Bundle("~/bundles/bootbox").Include(
+                     "~/Scripts/bootbox.min.js",
+                     "~/Scripts/bootbox.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
